03763bdc733d30daa09e3269f12790a7d0fef2d3
Lucas Saraiva <lucas.saraiva019@gmail.com>
2022-12-25 17:16:18 -0300

    adding a post component

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react'
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
@@ -7,11 +8,13 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/64505737?v=4" alt="" />
         <strong>Lucas Saraiva</strong>
         <span>Web Developer</span>
       </div>
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
