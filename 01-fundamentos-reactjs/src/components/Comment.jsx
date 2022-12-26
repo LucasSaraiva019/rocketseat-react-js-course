84c0dc45dd5ed3fdc7c51fef65d4f4d8d151faab
Lucas Saraiva <lucas.saraiva019@gmail.com>
2022-12-26 14:31:27 -0300

    adding comment component

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,30 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://avatars.githubusercontent.com/u/64505737?v=4" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Lucas Saraiva</strong>
+              <time title='11 de Maio ás 08:13' dateTime="2022-05-11 08:13:30">Cerca de 1h Atrás</time>
+            </div>
+            <button title='Deletar comentario'>
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
